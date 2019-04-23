fc5b1a28e9c431398e006769b1b54bf945c39774
ZhangYide <zhangfei2012@hotmail.com>
2019-04-23 18:30:35 +0800

    创建create_app函数，实现可采用不同config参数运行程序

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,7 +1,17 @@
 import os
 
 
-class Config():
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.office365.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
+class ConfigTest:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.office365.com'
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flaskblog import app
+from flaskblog import create_app
+from flaskblog.config import Config, ConfigTest
+
+app = create_app(Config)
 
 if __name__ == '__main__':
     app.run(debug=True)
